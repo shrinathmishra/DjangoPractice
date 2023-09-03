b07ab5a49170336bfd5ff04e05d33d72bae82c33
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-09-03 12:53:24 +0530

    Updating myapp

--- myapp/views.py
+++ myapp/views.py
@@ -60,3 +60,6 @@ def counter(request):
     text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
